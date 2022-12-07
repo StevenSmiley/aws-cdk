96723267adf6823744918e966effbe1bf21aedcb
Steven Smiley <smiley@scalesec.com>
2022-12-07 12:19:42 -0800

    feat: initial regexpatternset

---
+++ packages/@aws-cdk/aws-wafv2/lib/regex-pattern-set.ts
@@ -0,0 +1,26 @@
+import * as core from '@aws-cdk/core';
+import { Construct } from 'constructs';
+import { CfnRegexPatternSet } from './wafv2.generated';
+import { Scope } from './web-acl';
+
+export interface RegexPatternSetProps {
+  readonly name?: string;
+  readonly description?: string;
+  readonly scope: Scope;
+  readonly regularExpressionList: string[]
+  readonly tags?: core.Tag[];
+}
+
+export class RegexPatternSet extends core.Resource {
+  constructor(scope: Construct, id: string, props: RegexPatternSetProps) {
+    super(scope, id);
+
+    new CfnRegexPatternSet(this, 'Resource', {
+      name: props.name,
+      description: props.description,
+      scope: props.scope,
+      tags: props.tags,
+      regularExpressionList: props.regularExpressionList,
+    });
+  }
+}
