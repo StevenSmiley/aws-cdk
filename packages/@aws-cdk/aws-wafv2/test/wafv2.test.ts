2aca6c538d9328f8df532dc067a837ab4a0f9ccd
Steven Smiley <smiley@scalesec.com>
2022-12-15 11:17:30 -0800

    tests: plan

--- packages/@aws-cdk/aws-wafv2/test/wafv2.test.ts
+++ packages/@aws-cdk/aws-wafv2/test/wafv2.test.ts
@@ -1,14 +1,10 @@
 import { Template } from '@aws-cdk/assertions';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as cloudfrontorigins from '@aws-cdk/aws-cloudfront-origins';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as stepfunctions from '@aws-cdk/aws-stepfunctions';
 import { Stack } from '@aws-cdk/core';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as cloudfrontorigins from 'aws-cdk-lib/aws-cloudfront-origins';
-import * as cognito from 'aws-cdk-lib/aws-cognito';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as logs from 'aws-cdk-lib/aws-logs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
 import * as wafv2 from '../lib';
 
 describe('Web ACL scope', () => {
@@ -134,56 +130,367 @@ describe('Web ACL associations', () => {
 });
 
 describe('Managed rule groups', () => {
-  let stack: Stack;
-  beforeEach(() => {
-    stack = new Stack();
+  // let stack: Stack;
+  // beforeEach(() => {
+  //   stack = new Stack();
+  // });
+
+  test('AWS Managed rule group, default', () => {
+    // TODO
   });
 
-  test('todo', () => {
+  test('AWS Managed rule group, pin version', () => {
+    // TODO
+  });
+
+  test('AWS Managed rule group, override rule group action to count', () => {
+    // TODO
+  });
+
+  test('AWS Managed rule group, exclude rules', () => {
+    // TODO
+  });
+
+  test('AWS Managed rule group, scope-down rule', () => {
+    // TODO
+  });
+
+  test('AWS Marketplace rule group', () => {
     // TODO
   });
 });
 
 describe('Custom rule groups', () => {
-  let stack: Stack;
-  beforeEach(() => {
-    stack = new Stack();
+  // let stack: Stack;
+  // beforeEach(() => {
+  //   stack = new Stack();
+  // });
+
+  test('Rule action: ALLOW', () => {
+    // TODO
   });
 
-  test('todo', () => {
+  test('Rule action: ALLOW with custom headers and label', () => {
+    // TODO
+  });
+
+  test('Rule action: BLOCK', () => {
+    // TODO
+  });
+
+  test('Rule action: BLOCK with custom response and label', () => {
+    // TODO
+  });
+
+  test('Rule action: COUNT', () => {
+    // TODO
+  });
+
+  test('Rule action: COUNT with custom headers and label', () => {
+    // TODO
+  });
+
+  test('Rule action: CAPTCHA', () => {
+    // TODO
+  });
+
+  test('Rule action: CAPTCHA with custom immunity time, headers, and label', () => {
+    // TODO
+  });
+
+  test('Rule action: CHALLENGE', () => {
+    // TODO
+  });
+
+  test('Rule action: CHALLENGE with custom immunity time and label', () => {
+    // TODO
+  });
+
+  test('Statement: Originates from a county in, source IP', () => {
+    // TODO
+  });
+
+  test('Statement: Originates from a county in, IP address in header', () => {
+    // TODO
+  });
+
+  test('Statement: Originates from an IP address in, source IP', () => {
+    // TODO
+  });
+
+  test('Statement: Originates from an IP address in, IP address in header', () => {
+    // TODO
+  });
+
+  test('Statement: Has a label, match label', () => {
+    // TODO
+  });
+
+  test('Statement: Has a label, match label namespace', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, string match, exactly matches string', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, string match, starts with string', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, string match, ends with string', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, string match, contains string', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, string match, contains words', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, string match, pattern from regex pattern set', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, string match, regex', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, size match, equals', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, size match, not equal to', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, size match, less than or equal to', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, size match, less than', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, size match, greater than or equal to', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, size match, greater than', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, attack match, SQL injection', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single header, attack match, XSS injection', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect all headers', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect cookies', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect single query parameter', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect all query parameters', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect URI path', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect query string', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect body', () => {
+    // TODO
+  });
+
+  test('Statement: Inspect HTTP method', () => {
+    // TODO
+  });
+
+  test('Statement: Text transformations', () => {
+    // TODO
+  });
+
+  test('Match logic: AND', () => {
+    // TODO
+  });
+
+  test('Match logic: OR', () => {
+    // TODO
+  });
+
+  test('Match logic: NOT', () => {
+    // TODO
+  });
+
+  test('Combine rules into rule group', () => {
+    // TODO: AWS::WAFv2::RuleGroup https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-wafv2-rulegroup.html
+  });
+
+  test('Override rule group action to COUNT', () => {
+    // TODO: AWS::WAFv2::RuleGroup https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-wafv2-rulegroup.html
+  });
+});
+
+test('Regex Pattern Set', () => {
+  // TODO: AWS::WAFv2::RegexPatternSet https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-wafv2-regexpatternset.html
+});
+
+describe('IP Set', () => {
+  // let stack: Stack;
+  // beforeEach(() => {
+  //   stack = new Stack();
+  // });
+
+  test('IPv4', () => {
+    // TODO: AWS::WAFv2::IPSet https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-wafv2-ipset.html
+  });
+
+  test('IPv6', () => {
     // TODO
   });
 });
 
 describe('Default action', () => {
-  let stack: Stack;
-  beforeEach(() => {
-    stack = new Stack();
+  // let stack: Stack;
+  // beforeEach(() => {
+  //   stack = new Stack();
+  // });
+
+  test('Allow', () => {
+    // TODO
   });
 
-  test('todo', () => {
+  test('Allow with custom headers', () => {
+    // TODO
+  });
+
+  test('Block', () => {
+    // TODO
+  });
+
+  test('Block with custom response', () => {
     // TODO
   });
 });
 
 describe('Request sampling', () => {
-  let stack: Stack;
-  beforeEach(() => {
-    stack = new Stack();
+  // let stack: Stack;
+  // beforeEach(() => {
+  //   stack = new Stack();
+  // });
+
+  test('Default to enabled sampling', () => {
+    // TODO: AWS::WAFv2::WebACL VisibilityConfig SampledRequestsEnabled is true
   });
 
-  test('todo', () => {
+  test('Enabled sampling when specified', () => {
     // TODO
+  });
+
+  test('Disabled sampling when specified', () => {
+    // TODO
+  });
+
+  test('Enable with exclusions', () => {
+    // TODO: check default action sampling, rules
   });
 });
 
 describe('Logging', () => {
-  let stack: Stack;
-  beforeEach(() => {
-    stack = new Stack();
+  // let stack: Stack;
+  // beforeEach(() => {
+  //   stack = new Stack();
+  // });
+
+  test('No logging by default', () => {
+    // TODO
   });
 
-  test('todo', () => {
+  test('Send logs to CloudWatch Logs, new log group, with defaults', () => {
+    // TODO
+  });
+
+  test('Send logs to CloudWatch Logs, new log group, override defaults', () => {
+    // TODO
+  });
+
+  test('Send logs to CloudWatch Logs, provided log group', () => {
+    // TODO
+  });
+
+  test('Send logs to S3, new bucket, with defaults', () => {
+    // TODO
+  });
+
+  test('Send logs to S3, new bucket, override defaults', () => {
+    // TODO
+  });
+
+  test('Send logs to S3, provided bucket', () => {
+    // TODO
+  });
+
+  test('Send logs to Kinesis Firehose, new stream, with defaults', () => {
+    // TODO
+  });
+
+  test('Send logs to Kinesis Firehose, new stream, override defaults', () => {
+    // TODO
+  });
+
+  test('Send logs to Kinesis Firehose, provided stream', () => {
+    // TODO
+  });
+
+  test('Logging filter, default keep', () => {
+    // TODO
+  });
+
+  test('Logging filter, default drop', () => {
+    // TODO
+  });
+
+  test('Logging filter, filter condition, rule action', () => {
+    // TODO
+  });
+
+  test('Logging filter, filter condition, label', () => {
+    // TODO
+  });
+
+  test('Logging filter, keep if meets any', () => {
+    // TODO
+  });
+
+  test('Logging filter, keep if meets all', () => {
+    // TODO
+  });
+
+  test('Logging filter, drop if meets any', () => {
+    // TODO
+  });
+
+  test('Logging filter, drop if meets all', () => {
+    // TODO
+  });
+
+  test('Redacted fields', () => {
     // TODO
   });
 });
