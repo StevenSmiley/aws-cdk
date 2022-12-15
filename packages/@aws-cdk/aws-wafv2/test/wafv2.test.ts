df4a09152c09abb02aebe19ebb759b22b278694a
Steven Smiley <smiley@scalesec.com>
2022-12-15 08:41:25 -0800

    test: scope

--- packages/@aws-cdk/aws-wafv2/test/wafv2.test.ts
+++ packages/@aws-cdk/aws-wafv2/test/wafv2.test.ts
@@ -1,6 +1,26 @@
-import '@aws-cdk/assertions';
-import {} from '../lib';
+import { Template } from '@aws-cdk/assertions';
+import { Stack } from '@aws-cdk/core';
+import * as wafv2 from '../lib';
 
-test('No tests are specified for this package', () => {
-  expect(true).toBe(true);
+describe('Web ACL scope', () => {
+  let stack: Stack;
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  test('scope is CLOUDFRONT', () => {
+    new wafv2.WebACL(stack, 'WebACL', { scope: wafv2.Scope.CLOUDFRONT });
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties('AWS::WAFv2::WebACL', {
+      Scope: 'CLOUDFRONT',
+    });
+  });
+
+  test('scope is REGIONAL', () => {
+    new wafv2.WebACL(stack, 'WebACL', { scope: wafv2.Scope.REGIONAL });
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties('AWS::WAFv2::WebACL', {
+      Scope: 'REGIONAL',
+    });
+  });
 });
